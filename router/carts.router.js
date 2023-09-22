ea9703605066278f84eeae7b80737c31e20621b7
Santino Dorado <doradosantinotomas@gmail.com>
2023-09-22 11:22:44 -0300

    realizando mejoras

--- router/carts.router.js
+++ router/carts.router.js
@@ -3,11 +3,18 @@ import * as cartController from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
+//esta ruta la configure para poder ver si funcionaba
 router.get("/", (req,res)=>{
     res.send("hola bienvenido a la ruta de cart")
 } )
+
+// ruta para crear el carrito (http://localhost:8080/api/carts/)
 router.post("/", cartController.createCart);
+
+//ruta que permite obtener productos por id (http://localhost:8080/api/carts/{cid})
 router.get("/:cid", cartController.getCartById);
+
+// ruta para agregar un producto al carrito (http://localhost:8080/api/carts/{cid}/product/{pid})
 router.post("/:cid/product/:pid", cartController.addProductToCart);
 
 export default router;
--- router/products.router.js
+++ router/products.router.js
@@ -3,10 +3,19 @@ import { getProducts, getProductById, createProduct, updateProduct, deleteProduc
 
 const router = express.Router();
 
+// listado de los productos (http://localhost:8080/products/)
 router.get("/", getProducts);
+
+// filtrar producto por id (http://localhost:8080/products/{id})
 router.get("/:pid", getProductById);
+
+//crear producto (http://localhost:8080/products/)
 router.post("/", createProduct);
+
+//actualizar productos (http://localhost:8080/products/{id})
 router.put("/:pid", updateProduct);
+
+//esta ruta es la encargada de eliminar un producto (http://localhost:8080/products/{id})
 router.delete("/:pid", deleteProduct);
 
 export default router;
